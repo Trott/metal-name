c7bb8b72341c3027c5ab086bc0d4f99933ea00cc
Rich Trott <rtrott@gmail.com>
2021-12-27 17:51:13 +0000

    chore: remove meow dependency
    I was only using it for a help message in the CLI. No more help message.
    Sorry. There are no options anyway. I suppose someone might consider
    this a breaking change. For this module, I do not.

--- cli.js
+++ cli.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 'use strict'
-const meow = require('meow')
 const metal = require('./')
-
-meow({
-  help: [
-    'Examples',
-    '  $ metal-name',
-    '  Desecrated Glory'
-  ].join('\n')
-})
 
 console.log(metal())
